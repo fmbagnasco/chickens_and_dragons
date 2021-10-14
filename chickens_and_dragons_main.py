b81ba52d931a71ab36f57f86c88fbf479ff1a7cf
fmbagnasco <fmbagnasco@me.com>
2021-10-14 11:55:09 +0200

    added first in game desc and choices

--- chickens_and_dragons_main.py
+++ chickens_and_dragons_main.py
@@ -1,2 +1,32 @@
 from chickens_and_dragons_classes import *
 
+print("""Welcome to Chickens & Dragons,
+the only game that takes place in the
+wonderfult Chickens & Dragons Universe!
+
+Please select a race for your hero!
+The choices are:
+- human
+- orc
+- elf
+
+make sure you spell the race correctly!""")
+
+user_race = input()
+
+print("Fantastic! You picked " + user_race + "!")
+print("""Now it's time to pick your hero class!
+
+The different choices are:
+- barbarian
+- paladin
+- scout
+
+Think about this carefully, because your class will
+determine which weapons and armors you will
+be most proficient in!""")
+
+user_class = input()
+
+print("Great! you chose " + user_class + "!")
+print("Now that you are all set up with your hero, let's get started!")
